f3ec3119c259de387a3aa65fdb0d0a852490fe1f
Suhun Han <ssut@ssut.me>
2019-04-02 00:03:52 +0900

    fix(test): except typescript-related files

--- jest.config.js
+++ jest.config.js
@@ -143,9 +143,11 @@ module.exports = {
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+    "/node_modules/",
+    ".d.ts",
+    "/lib/"
+  ],
 
   // The regexp pattern Jest uses to detect test files
   // testRegex: "",
