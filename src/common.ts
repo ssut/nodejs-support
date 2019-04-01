859664eb9570c26522b45684acc4469453579695
Suhun Han <ssut@ssut.me>
2019-04-02 00:04:51 +0900

    fix(common): working

--- src/common.ts
+++ src/common.ts
@@ -1,30 +1,35 @@
+import * as _ from 'lodash';
+
 /** @private */
 export function isDefined(value: any) {
-    return typeof value !== 'undefined' && value !== null;
+  return typeof value !== 'undefined' && value !== null;
 }
 
 /** @private */
 export function getOrUndefined(value: any) {
-    return isDefined(value) ? value : undefined;
+  return isDefined(value) ? value : undefined;
 }
 
 /** @private */
 export function assert(condition: any, msg: string) {
-    if (!condition)
-        throw TypeError(msg);
+  if (!condition)
+    throw TypeError(msg);
 }
 
 /** @private */
-export function typeCheck(values: any[], ...types: any[]) {
-    const _types = new Set(types);
+export function typeCheck(values: any[], ...types: string[]) {
+  const _types = new Set(types);
 
-    new Set(values.map((x) => {
-        let t = typeof x
-        if (t === 'object')
-            t = x.constructor.name;
+  new Set(
+    values.map((value) => {
+      let type: string = typeof value;
+      if (type === 'object') {
+        type = value.constructor.name;
+      }
 
-        return t;
-    })).forEach((t) => {
-        assert(_types.has(t), `[${Array.from(_types).toString()}] 중 하나를 기대했지만 ${t} 타입이 들어왔습니다!`);
-    });
+      return type;
+    })
+  ).forEach((t) => {
+    assert(_types.has(t), `[${Array.from(_types).toString()}] 중 하나를 기대했지만 ${t} 타입이 들어왔습니다!`);
+  });
 }
